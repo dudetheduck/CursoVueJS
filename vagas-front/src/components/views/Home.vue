f38fd958f5d82ad1e7686f9f0b43056544223aa6
Dudu Santos <Dudu Santos>
2022-03-17 14:59:44 -0300

    VueJS Day 14

--- vagas-front/src/components/views/Home.vue
+++ vagas-front/src/components/views/Home.vue
@@ -6,18 +6,31 @@
       </div>
     </div>
 
+    <div class="row mt-5" v-for="(vaga, index) in vagas" :key="index">
+      <div class="col">
+        <vaga
+          :titulo-vaga-teste="vaga.titulo"
+          :descricao-vaga="vaga.descricao"
+          :salario="vaga.salario"
+          :modalidade="vaga.modalidade"
+          :tipo="vaga.tipo"
+          :publicacao="vaga.publicacao"
+        />
+      </div>
+    </div>
+
     <div class="row mt-5">
 
       <div class="col-4">
-        <indicador></indicador>
+        <indicador titulo="Vagas abertas" indicador="100" bg="bg-dark" color="text-white"></indicador>
       </div>
 
       <div class="col-4">
-        <indicador></indicador>
+        <indicador titulo="Profissionais cadastrados" indicador="225" bg="bg-dark" color="text-white"></indicador>
       </div>
 
       <div class="col-4">
-        <indicador></indicador>
+        <indicador  titulo="Visitantes online" :indicador="usuariosOnline" bg="bg-light" color="text-dark"></indicador>
       </div>
 
     </div>
@@ -27,28 +40,68 @@
 <script>
 import Indicador from '@/components/comuns/Indicador.vue'
 import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue'
+import Vaga from '@/components/comuns/Vaga.vue'
 
   export default {
     name: 'Home',
     components: {
       PesquisarVaga,
-      Indicador
+      Indicador,
+      Vaga
     },
-    activated() {
-      console.log('Componente é ativado')
-    },
-    deactivated() {
-      console.log('Desativado')
-    },
-    beforeUnmount() {
-      console.log('Antes de desmontar/destruir')
-    },
-    unmounted() {
-      console.log('Desmontado/destruido')
+    data: () => ({
+      usuariosOnline: 0,
+      vagas: [
+      {
+        titulo: 'Analista Programador PHP Pleno',
+        descricao: 'Profissional com conhecimentos em PHP, Laravel e MySQL. Necessário 3 anos de experiências. Atuará na manutenção de sistemas legados da empresa.',
+        salario: 6000,
+        modalidade: 'Home Office',
+        tipo: 'PJ',
+        publicacao: '2021-10-10'
+      },
+      {
+        titulo: 'Programador JavaScript Angular',
+        descricao: 'Profissional com conhecimentos avançados em JavaScript e Angular.',
+        salario: 5000,
+        modalidade: 'Presencial',
+        tipo: 'CLT',
+        publicacao: '2021-10-07'
+      },
+      {
+        titulo: 'Programador JavaScript Vue',
+        descricao: 'Profissional com conhecimentos avançados em JavaScript e Vue.',
+        salario: 5000,
+        modalidade: 'Home Office',
+        tipo: 'CLT',
+        publicacao: '2021-10-06'
+      },
+      {
+        titulo: 'Analista de Banco de Dados Sênior',
+        descricao: 'Domínio dos bancos de dados SQL Server, Oracle, Postgre e MySQL',
+        salario: 9000,
+        modalidade: 'Presencial',
+        tipo: 'PJ',
+        publicacao: '2021-10-06'
+      },
+      {
+        titulo: 'Programador Web Júnior',
+        descricao: 'Conhecimentos básicos em HTML, CSS, JavaScript, Bootstrap, PHP e MySQL',
+        salario: 3000,
+        modalidade: 'Presencial',
+        tipo: 'CLT',
+        publicacao: '2021-10-05'
+      }
+    ]
+    }),
+    methods: {
+      getUsuariosOnline() {
+        this.usuariosOnline = Math.floor(Math.random() * 101) // Entre 0 e 100
+      }
     },
     created() {
-      console.log('Criado', this.teste)
-    },
+      setInterval(this.getUsuariosOnline, 1000) // A cada 1seg
+    }
   }
 </script>
 
