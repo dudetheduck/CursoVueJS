ab0b647484c58a809d7e845a583cc24c55026c2d
Dudu Santos <Dudu Santos>
2022-03-19 14:24:01 -0300

    VueJS Day 17

--- vagas-front/src/components/comuns/Vaga.vue
+++ vagas-front/src/components/comuns/Vaga.vue
@@ -1,18 +1,72 @@
 <template>
-    <div class="card">
-          <div class="card-header bg-dark text-white">{{ tituloVagaTeste }}</div>
-          <div class="card-body">
-            <p>{{ descricaoVaga }}</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Salário: R$ {{ salario }} | Modalidade: {{ modalidade }} | Tipo: {{ tipo }} | Publicação: {{ publicacao }}</small>
-          </div>
-        </div>
+  <div class="card">
+    <div class="card-header bg-dark text-white">{{ titulo }}</div>
+    <div class="card-body">
+      <p>{{ descricao }}</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-muted">Salário: R$ {{ salario }} | Modalidade: {{ getModalidade }} | Tipo: {{ getTipo }} | Publicação: {{ getPublicacao }}</small>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Vaga',
-        props: ['tituloVagaTeste', 'descricaoVaga', 'salario', 'modalidade', 'tipo', 'publicacao']
+export default {
+  name: 'Vaga',
+  // props: ['tituloVagaTeste', 'descricaoVaga', 'salario', 'modalidade', 'tipo', 'publicacao'],
+  props: {
+    titulo: {
+      type: String,
+      required: true,
+      validator(p) {
+        // console.log('Prop: ', p)
+        if(p.length < 6) return false // Se estiver inválido
+        return true // Se estiver válido
+      }
+    },
+    descricao: {
+      type: String,
+      // default: 'O contratante não adicionou uma descrição para essa vaga'
+      default() {
+        return '*'.repeat(20)
+      }
+    },
+    salario: {
+      type: [ Number, String ],
+      required: true
+    },
+    modalidade: {
+      type: String,
+      required: true
+    },
+    tipo: {
+      type: String,
+      required: true
+    },
+    publicacao: {
+      type: String,
+      required: true
     }
+  },
+  computed: {
+    getModalidade() {
+      switch(this.modalidade) {
+        case '1': return 'Home Office'
+        case '2': return 'Presencial'
+      }
+      return ''
+    },
+    getTipo() {
+      switch(this.modalidade) {
+        case '1': return 'CLT'
+        case '2': return 'PJ'
+      }
+      return ''
+    },
+    getPublicacao() {
+      let dataPublicacao = new Date(this.publicacao)
+      return dataPublicacao.toLocaleDateString('pt-BR')
+    }
+  }
+}
 </script>
