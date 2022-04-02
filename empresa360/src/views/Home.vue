e9b98701aad043cfd5107ccae4946426bf037a4a
Dudu Santos <Dudu Santos>
2022-04-02 09:18:56 -0300

    VueJS Day 30

---
+++ empresa360/src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h3>Home</h3>
+        <router-link to="/login">Navegar para "/login"</router-link>
+    </div>
+</template>
---
+++ empresa360/src/views/Login.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h3>Login</h3>
+        <router-link :to="{ path: '/home' }">Navegar para "/home"</router-link>
+    </div>
+</template>
