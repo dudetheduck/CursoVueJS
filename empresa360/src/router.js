adce703e3f4a1c6c7837b77f8358045e8aa56d57
Dudu Santos <Dudu Santos>
2022-04-04 01:04:15 -0300

    VueJS Day 32

--- empresa360/src/router.js
+++ empresa360/src/router.js
@@ -1,12 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // createWebHashHistory
+import Contratos from '@/components/vendas/Contratos.vue'
+import Dashboard from '@/components/dashboard/Dashboard.vue'
 import Home from '@/views/Home.vue'
+import Leads from '@/components/vendas/Leads.vue'
 import Login from '@/views/Login.vue'
+import Servicos from '@/components/servicos/Servicos.vue'
+import Site from '@/views/Site.vue'
+import Vendas from '@/components/vendas/Vendas.vue'
 
 const routes = [
+    {
+        path: '/', // localhost:8080
+        component: Site
+    },
     { 
         path: '/home', // localhost:8080/home
-        component: Home
+        component: Home,
+        children: [
+            { path: 'vendas', component: Vendas, children: 
+                [
+                    { path: 'leads', component: Leads }, // localhost:8080/home/vendas/leads
+                    { path: 'contratos', component: Contratos }, // localhost:8080/home/vendas/contratos
+                ]
+            }, // localhost:8080/home/vendas
+            { path: 'servicos', component: Servicos }, // localhost:8080/home/servicos
+            { path: 'dashboard', component: Dashboard } // localhost:8080/home/dashboard
+        ]
     },
     {
         path: '/login', // localhost:8080/login
